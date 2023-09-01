2d9b3fd62f885f2bd6092644e92a5da5d2412592
Anna-1987 <mag260910@gmail.com>
2023-09-01 17:50:38 +0300

    home-1

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,37 @@
+import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
+
+const getRandomHexColor = () => {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+
+export const Statistics = ({title, stats}) => {
+    return (
+        <section className={css.statistics}>
+  <h2 className={css.title}>{title}</h2>
+
+  <ul className={css.stats__list}>
+  {stats.map(({ id, label, percentage }) => {
+          return (
+            <li
+              className={css.stats}
+              key={id}
+              style={{ backgroundColor: getRandomHexColor() }}
+            >
+              <span className={css.label}>{label}</span>
+              <span className={css.percentage}>{percentage}</span>
+            </li>
+          );
+        })}
+ </ul>
+</section>
+    );
+
+};
+
+// Statistics.PropTypes = {
+//  label: propTypes.string.isRequired,
+//  percentage: propTypes.number.isRequired,
+// }
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,42 @@
+import css from './TransactionHistory.module.css';
+import PropTypes from 'prop-types';
+
+const getRandomHexColor = () => {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+
+export const TransactionHistory = ({items}) => {
+
+ return (
+    <table>
+      <thead>
+        <tr style={{ backgroundColor: getRandomHexColor() }}>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => {
+          return (
+            <tr key={id} style={{ backgroundColor: getRandomHexColor() }}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+TransactionHistory.propTypes = {
+  id: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  amount: PropTypes.string.isRequired,
+  currency: PropTypes.string.isRequired,
+};
