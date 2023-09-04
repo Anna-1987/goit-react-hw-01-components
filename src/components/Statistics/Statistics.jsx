d6c8a1845b6970ccfdfb8bde68d04adbccb3d984
Anna-1987 <mag260910@gmail.com>
2023-09-04 08:28:09 +0300

    home-1-rectification

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -10,7 +10,8 @@ const getRandomHexColor = () => {
 export const Statistics = ({title, stats}) => {
     return (
         <section className={css.statistics}>
-  <h2 className={css.title}>{title}</h2>
+ 
+  {title && <h2 className={css.title}>{title}</h2>}
 
   <ul className={css.stats__list}>
   {stats.map(({ id, label, percentage }) => {
